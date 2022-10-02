6508ad25bfb693e417c369d772bd8d64fac4dc3b
danko1900 <daniel.gavras@outlook.com>
2022-10-02 00:42:47 -0400

    Final changes Part 3

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,14 +5,14 @@ class TestTranslator(unittest.TestCase):
     '''Tests for english_to_french method.'''
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hi, my name is Dan!'),'Salut, mon nom est Dan !')
-        self.assertEqual(english_to_french('I don\'t speak French.'),'Je ne parle pas français.')
+        self.assertNotEqual(english_to_french('Goodbye'),'Goodbye')
         self.assertEqual(english_to_french(''),'Text must be provided.')
         self.assertEqual(english_to_french(None),'Text must be provided.')
         self.assertEqual(english_to_french('Hello'),'Bonjour')
     '''Tests for french_to_english method.'''
     def test_french_to_english(self):
         self.assertEqual(french_to_english('Salut, mon nom est Dan !'),'Hi, my name is Dan!')
-        self.assertEqual(french_to_english('Je ne parle pas français.'),'I don\'t speak French.')
+        self.assertNotEqual(french_to_english('Au Revoir'),'Au Revoir')
         self.assertEqual(french_to_english(''),'Text must be provided.')
         self.assertEqual(french_to_english(None),'Text must be provided.')
         self.assertEqual(french_to_english('Bonjour'),'Hello')  
